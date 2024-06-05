1fc91080454115feb4fe02fc8e61d60942c34db3
Wenchenesse <wencheeh99j@gmail.com>
2024-06-05 16:55:37 +0300

    stats for Lyon

--- parsing.py
+++ parsing.py
@@ -8,6 +8,22 @@ from selenium.webdriver.support import expected_conditions as EC
 import traceback
 import pandas as pd
 
+links = ['https://fbref.com/en/matches/13878f62/Le-Havre-Lyon-September-15-2023-Division-1-Feminine',
+         'https://fbref.com/en/matches/da74f2fe/Paris-Saint-Germain-Lyon-October-1-2023-Division-1-Feminine',
+         'https://fbref.com/en/matches/c577e576/Lyon-Bordeaux-October-8-2023-Division-1-Feminine',
+         'https://fbref.com/en/matches/78027557/Lyon-Saint-Etienne-October-14-2023-Division-1-Feminine',
+         'https://fbref.com/en/matches/6df32e03/Stade-de-Reims-Lyon-October-22-2023-Division-1-Feminine',
+         'https://fbref.com/en/matches/040b32bb/Paris-FC-Lyon-November-5-2023-Division-1-Feminine',
+         'https://fbref.com/en/matches/faf3df46/Lyon-Montpellier-November-10-2023-Division-1-Feminine',
+         'https://fbref.com/en/matches/51145236/Slavia-Prague-Lyon-November-14-2023-Champions-League',
+         'https://fbref.com/en/matches/7214e79d/Lyon-Dijon-November-17-2023-Division-1-Feminine',
+         'https://fbref.com/en/matches/49db0aff/Lyon-St-Polten-November-22-2023-Champions-League',
+         'https://fbref.com/en/matches/013c6622/Guingamp-Lyon-November-26-2023-Division-1-Feminine',
+         'https://fbref.com/en/matches/6da4bd3a/Lyon-Lille-December-8-2023-Division-1-Feminine',
+         'https://fbref.com/en/matches/af4b1ba3/Lyon-Brann-December-13-2023-Champions-League',
+         'https://fbref.com/en/matches/7ebb0d27/Fleury-Lyon-December-16-2023-Division-1-Feminine',
+         'https://fbref.com/en/matches/db4a3116/Brann-Lyon-December-21-2023-Champions-League',
+         'https://fbref.com/en/matches/cd755028/Barcelona-Lyon-May-25-2024-Champions-League']
 
 columns=['Player', 'Pos', 'Age', 'Min', 'Gls', 'Ast', 'Sh', 'SoT', 'CrdY', 'CrdR', 'Touches', 'Tkl', 'Int',
              'Blocks', 'SCA', 'GCA', 'Cmp', 'Att', 'cpm%', 'PrgP', 'PrgC', 'TOatt', 'TOsucc']
@@ -33,38 +49,40 @@ datastat = ['position','age', 'minutes','goals','assists','shots','shots_on_targ
 driver.set_page_load_timeout(90)
 
 try:
-    driver.get('https://fbref.com/en/matches/da74f2fe/Paris-Saint-Germain-Lyon-October-1-2023-Division-1-Feminine')
-    players = WebDriverWait(driver, 60).until(
-        EC.presence_of_all_elements_located((By.XPATH, '//th[@data-stat="player"]'))
-    )
-    # extract player names
-    players_list1 = [player.text.strip() for player in players if player.text != ""]
-    for name in players_list1:
-        if name == "Player" or name == "15 Players":
-            players_list1.remove(name)
-    players_list = []
-    for x in players_list1:
-        if x not in players_list:
-            players_list.append(x)
-    print(players_list)
-    player_stat ={'players': players_list}
-    for elem in datastat:
-        player_stat[elem] = WebDriverWait(driver, 60).until(
-        EC.presence_of_all_elements_located((By.XPATH, f'//td[@data-stat="{elem}"]'))
+    for link in links:
+        driver.get(link)
+        players = WebDriverWait(driver, 60).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//th[@data-stat="player"]'))
         )
-    players_stats ={}
-    for key, values in player_stat.items():
-        if key == 'players':
-            players_stats[key] = values
-        else:
-            players_stats[key] = [value.text for value in values if hasattr(value, 'text') and value.text != ""]
+        # extract player names
+        players_list1 = [player.text.strip() for player in players if player.text != ""]
+        for name in players_list1:
+            if name == "Player" or name == "15 Players":
+                players_list1.remove(name)
+        players_list = []
+        for x in players_list1:
+            if x not in players_list:
+                players_list.append(x)
+        print(players_list)
+        player_stat ={'players': players_list}
+        for elem in datastat:
+            player_stat[elem] = WebDriverWait(driver, 60).until(
+            EC.presence_of_all_elements_located((By.XPATH, f'//td[@data-stat="{elem}"]'))
+            )
+        players_stats ={}
+        for key, values in player_stat.items():
+            if key == 'players':
+                players_stats[key] = values
+            else:
+                players_stats[key] = [value.text for value in values if hasattr(value, 'text') and value.text != ""]
 
-    for key, values in players_stats.items():
-      print(f"{key}: {[value for value in values]}")
+        for key, values in players_stats.items():
+            print(f"{key}: {[value for value in values]}")
 
-    complete_stats = list(zip(*players_stats.values()))
-    print(complete_stats)
-    df = pd.DataFrame(complete_stats, columns=columns)
+        complete_stats = list(zip(*players_stats.values()))
+        print(complete_stats)
+        temp_df = pd.DataFrame(complete_stats, columns=columns)
+        df = df.append(temp_df)
 
     # save dataframe to csv
     df.to_csv('player_stats.csv', index=False, encoding='utf-8-sig')
